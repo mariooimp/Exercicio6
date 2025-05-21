7c8513ea9c0cc8b6a5b7ac1c510056887351e029
mario <mario@10.146.164.117>
2025-05-20 22:35:09 -0300

    teste

---
+++ src/exercicio6/Exercicio6.java
@@ -0,0 +1,12 @@
+package exercicio6;
+
+import javax.swing.JOptionPane;
+public class Exercicio6 {
+
+    public static void main(String[] args) {
+        int numero;
+        numero = 20;
+        JOptionPane.showMessageDialog(null, "numero escolhido: " +numero);
+        
+    }
+}
